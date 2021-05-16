956f608d41019ede7777966a64fa59c2d6352a24
Kritika Saxena <82437949+Kritika05802@users.noreply.github.com>
2021-05-16 20:33:38 +0530

    by Kritika Saxena

---
+++ decisionmaking-checkpoint.py
@@ -0,0 +1,80 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#program to print all positive numbers in a range"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "12\n",
+      "5\n",
+      "64\n",
+      "3\n"
+     ]
+    }
+   ],
+   "source": [
+    "l1= [12, -7, 5, 64, -14, 3, -4]\n",
+    "for num in l1:\n",
+    "    if num>0:\n",
+    "        print(num)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "12\n",
+      "14\n",
+      "3\n",
+      "24\n"
+     ]
+    }
+   ],
+   "source": [
+    "l2= [12, 14, -95, 3, 24, -5]\n",
+    "for num in l2:\n",
+    "    if num>0:\n",
+    "        print(num)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
